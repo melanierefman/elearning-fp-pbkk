4caab70b140ce096d964db9e8d1d417f415d1dfa
melanierefman <87106838+melanierefman@users.noreply.github.com>
2023-12-12 07:23:07 +0700

    update page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,12 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/books_users', 'User::books_users');
-$routes->get('/chart_users', 'User::chart_users');
-$routes->get('/login', 'Auth::login');
-$routes->get('/login2', 'Auth::login2');
-$routes->get('/register', 'Auth::register');
-$routes->get('/welcome', 'Welcome::welcome');
-$routes->get('/admin_books', 'Admin::admin_books');
-$routes->get('/add_books', 'Admin::add_books');
-$routes->get('/edit_books', 'Admin::edit_books');
+$routes->get('/kelas', 'Siswa::kelas');
+$routes->get('/jadwal', 'Siswa::jadwal');
+$routes->get('/add_jadwal', 'Siswa::add_jadwal');
---
+++ app/Controllers/Siswa.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class Siswa extends BaseController
+{
+    public function kelas(): string
+    {
+        return view('kelas');
+    }
+
+    public function jadwal(): string
+    {
+        return view('jadwal');
+    }
+
+    public function add_jadwal(): string
+    {
+        return view('add_jadwal');
+    }
+}
